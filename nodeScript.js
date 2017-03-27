ebdf25eedb952aeda2dba687ffea8494c27ed5c9
Tiffany Pan <t1pan@ucsd.edu>
2017-03-27 16:55:17 -0700

    working

--- nodeScript.js
+++ nodeScript.js
@@ -4,3 +4,23 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017';
 
+var insertDocument = function(db, callback) {
+   db.collection('fonts').insertOne( {
+     "font_id": ObjectId(),
+     "font_name": "Helvetica",
+     "file": "helvetica.file",
+    "web": "https://fontaddress.com",
+     "preview": "http://preview.it"
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback();
+  });
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  insertDocument(db, function() {
+      db.close();
+  });
+});
